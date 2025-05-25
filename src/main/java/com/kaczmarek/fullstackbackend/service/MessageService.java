11d4c2312fd91fc233275dd3b9cc93cac59ec141
Jacek <jacek.kaczmarek@cornelsen.de>
2025-05-25 19:04:46 +0200

    refactor: format code

--- src/main/java/com/kaczmarek/fullstackbackend/service/MessageService.java
+++ src/main/java/com/kaczmarek/fullstackbackend/service/MessageService.java
@@ -7,6 +7,7 @@ import com.kaczmarek.fullstackbackend.model.Message;
 import com.kaczmarek.fullstackbackend.repository.MessageRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.text.StringEscapeUtils;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -20,18 +21,41 @@ public class MessageService {
     private final MessageRepository repository;
 
     public MessageDto save(NewMessageDto newMessageDto) {
-        final var input = newMessageDto.getContent();
-        final var sanitized = org.apache.commons.text.StringEscapeUtils.escapeHtml4(input);
-        log.info("Saving message: {}", sanitized);
+        final var sanitizedContent = sanitizeContent(newMessageDto.getContent());
+        logMessage(sanitizedContent);
+        final var savedMessage = saveMessage(sanitizedContent);
+        return convertToDto(savedMessage);
+    }
+
+    private String sanitizeContent(String content) {
+        return StringEscapeUtils.escapeHtml4(content);
+    }
+
+    private void logMessage(String content) {
+        log.info("Saving message: {}", content);
+    }
+
+    private Message saveMessage(String content) {
         var message = new Message();
-        message.setContent(sanitized);
-        message = repository.save(message);
+        message.setContent(content);
+        return repository.save(message);
+    }
+
+    private MessageDto convertToDto(Message message) {
         return mapper.toDto(message);
     }
 
     public List<MessageDto> getAll() {
-        log.debug("Fetching all messages");
+        logFetchingAllMessages();
         final var messages = repository.findAll();
+        return convertToDtoList(messages);
+    }
+
+    private void logFetchingAllMessages() {
+        log.debug("Fetching all messages");
+    }
+
+    private List<MessageDto> convertToDtoList(List<Message> messages) {
         return mapper.toDtoList(messages);
     }
 }
